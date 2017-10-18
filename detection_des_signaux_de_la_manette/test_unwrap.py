d64dea326fb42cd5077ed87c94f35953426cf974
OliJD <32197926+OliJD@users.noreply.github.com>
2017-10-18 09:32:34 -0400

    Ajout d'un test pour un module d'unwrap de module

---
+++ detection_des_signaux_de_la_manette/test_unwrap.py
@@ -0,0 +1,37 @@
+"""
+Embedded Python Blocks:
+
+Each time this file is saved, GRC will instantiate the first class it finds
+to get ports and parameters of your block. The arguments to __init__  will
+be the parameters. All of them are required to have default values!
+
+
+version modifie de http://aaronscher.com/GNU_Radio_Companion_Collection/Embedded_Python_Example1.html
+"""
+
+import numpy as np
+from gnuradio import gr
+
+
+class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+    """Embedded Python Block example - a simple multiply const"""
+
+    def __init__(self, example_param=1):  # only default arguments here
+        """arguments to this function show up as parameters in GRC"""
+        gr.sync_block.__init__(
+            self,
+            name='Embedded Python Block',   # will show up in GRC
+            in_sig=[np.float32],
+            out_sig=[np.float32]
+        )
+        # if an attribute with the same name as a parameter is found,
+        # a callback is registered (properties work, too).
+        self.example_param = example_param
+
+    def work(self, input_items, output_items):
+        """example: multiply with constant"""
+		phase =np.linspace(0,0,num=1)
+		phase[0]=input_items[0]
+		np.unwrap(phase)
+		output_item[0][:]=phase[0]
+        return len(output_items[0])
